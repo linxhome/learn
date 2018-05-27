fbebc4a31e6d4338be8623e9b89ed7fc53b6eef5
linxhome <linxome@gmail.com>
2018-05-27 18:07:53 +0800

    del

--- android_project/reader/app/src/main/java/reader/newbird/com/book/IGetBook.java
+++
@@ -1,8 +0,0 @@
-package reader.newbird.com.book;
-
-import java.util.List;
-
-public interface IGetBook {
-    void updateBook(BookModel data);
-    void updateBooks(List<BookModel> data);
-}
--- android_project/reader/txtparse/src/main/java/com/newbird/parse/config/TextMeasureSize.java
+++
@@ -1,6 +0,0 @@
-package com.newbird.parse.config;
-
-public class TextMeasureSize {
-    public int height;
-    public int width;
-}
